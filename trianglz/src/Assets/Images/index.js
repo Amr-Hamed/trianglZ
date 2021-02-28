e5364f6859626049e5ff6d73c7a8cec3debfe81f
Amr <amr.hamed@vodafone.com>
2021-02-28 22:34:28 +0200

    restructuring assets folder

--- trianglz/src/Assets/Images/index.js
+++ trianglz/src/Assets/Images/index.js
@@ -1,67 +1,9 @@
-import about from './about.jpg'
-import clients from './clients.jpg'
-import contacts from './contact.jpg'
-import homeTitle from './home-title.png'
-import ipad from './ipad.png'
-import landing from './landing.jpg'
-import logo from './logo.png'
-import projectBazzar from './project-bazaar.jpg'
-import projectBridge from './project-bridge.jpg'
-import projectColumn from './project-column.jpg'
-import projectEnclinic from './project-enclinic.jpg'
-import projectIndoor from './project-indoor.jpg'
-import projectLife from './project-life.jpg'
-import projectRepair from './project-repair.jpg'
-import projectSelena from './project-selena.jpg'
-import projectShopper from './project-shopper.jpg'
-import projectSurvey from './project-survey.jpg'
-import sampleProject from './sample-project.jpg'
-import scissorsImage from './scissors-image.jpg'
-import selenaIpad from './selena-ipad.png'
-import selenaIphone from './selena-iphone.png'
-import selenaMac from './selena-mac.png'
-import selenaWatch from './selena-watch.png'
-import services from './services.jpg'
-import team from './team.jpg'
-import testimonials from './testimonials.jpg'
-import watch from './watch.png'
-import workEnvOne from './work-env-1.jpg'
-import workEnvTwo from '././work-env-2.jpg'
-import workEnvThree from '././work-env-3.jpg'
-import workEnvFour from '././work-env-4.jpg'
+import icons from './Icons'
+import images from './Images'
+import logos from './Logos'
 
-const images = {
-  about,
-  clients,
-  contacts,
-  homeTitle,
-  ipad,
-  landing,
-  logo,
-  projectBazzar,
-  projectBridge,
-  projectColumn,
-  projectEnclinic,
-  projectIndoor,
-  projectLife,
-  projectRepair,
-  projectSelena,
-  projectShopper,
-  projectSurvey,
-  sampleProject,
-  scissorsImage,
-  selenaIpad,
-  selenaIphone,
-  selenaMac,
-  selenaWatch,
-  services,
-  team,
-  testimonials,
-  watch,
-  workEnvOne,
-  workEnvTwo,
-  workEnvThree,
-  workEnvFour
+export {
+  icons,
+  images,
+  logos
 }
-
-export default images
